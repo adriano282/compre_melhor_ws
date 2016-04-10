4de3ecdc0389531600b1222a2d234a258e8c753f
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-04-09 21:58:23 -0300

    Fixes for use of API on Android

--- src/main/java/com/compremelhor/ws/resource/impl/StockResource.java
+++ src/main/java/com/compremelhor/ws/resource/impl/StockResource.java
@@ -3,14 +3,24 @@ package com.compremelhor.ws.resource.impl;
 import java.io.InputStream;
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.naming.NamingException;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
+import com.compremelhor.model.entity.Sku;
 import com.compremelhor.model.entity.Stock;
 import com.compremelhor.model.exception.InvalidEntityException;
+import com.compremelhor.model.exception.UnknownAttributeException;
 import com.compremelhor.model.remote.EJBRemote;
 import com.compremelhor.ws.annotation.TokenAuthenticated;
 import com.compremelhor.ws.resource.AbstractResource;
@@ -47,5 +57,23 @@ public class StockResource extends AbstractResource<Stock> {
 			return Response.status(406).entity(Entity.json(e.getMessage())).build();
 		}
 	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Stock getResourceByAttribute(@Context UriInfo info) {
+		Map<String, String> map = new HashMap<String,String>();
+		info.getQueryParameters(true).forEach((s, v) -> map.put(s, v.get(0)));
+		Stock u = null;
+		try {
+			u  = service.find(map);
+		} catch (UnknownAttributeException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		if (u == null) throw new WebApplicationException(Response.Status.NOT_FOUND);
+		return u;
+	}
+
 
 }
